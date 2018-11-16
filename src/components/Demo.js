0118752d0c552c7d1d63db68a53966f5f08021fe
Brendan Moore <brendan.moore@rangle.io>
2018-11-16 16:15:52 -0500

    Add mdx deck

---
+++ src/components/Demo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Demo = () => (
+  <div>
+    <h3>Demo!</h3>
+    <span>This is a pretty simple demo</span>
+    <pre>but I like it.</pre>
+  </div>
+);
+
+export default Demo;
